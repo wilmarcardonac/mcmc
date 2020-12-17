056d917336105815030532e402f0b378c6629998
Wilmar Alberto Cardona Castro <wilmar.cardona@unige.ch>
2020-12-17 15:48:43 -0500

    550000 STEPS, 50000 BEFORE FREEZING OUT COVMAT, 20 TO UPDATE COVMAT WITH JUMPING FACTOR, 2000 TO COMPUTE NEW COVMAT, COVMAT CHANGES BY FACTOR 0.1

--- input.f90
+++ input.f90
@@ -25,9 +25,9 @@ Module input
   ! MCMC PARAMETERS 
   Character(len=*),parameter :: likelihood = 'euclid' ! OPTIONS: 'gaussian','euclid'
   Character(len=*),parameter :: starting_point = 'mean' ! OPTIONS: 'mean','bestfit','random','last_point'
-  Character(len=*),parameter :: starting_cov_mat = 'given' !'diagonal' ! OPTIONS: 'diagonal','given'
+  Character(len=*),parameter :: starting_cov_mat = 'diagonal' !'diagonal' ! OPTIONS: 'diagonal','given'
   
-  Integer*4,parameter :: number_iterations = 110000 ! TOTAL NUMBER OF ITERATIONS IN MCMC RUN
+  Integer*4,parameter :: number_iterations = 550000 ! TOTAL NUMBER OF ITERATIONS IN MCMC RUN
   Integer*4,parameter :: number_of_parameters = 10   ! TOTAL NUMBER VARYING PARAMETERS     
   Integer*4,parameter :: UNIT_FILE1 = 80  ! UNIT NUMBER EXECUTION INFORMATION FILE
   Integer*4,parameter :: UNIT_FILE2 = 81  ! UNIT NUMBER GETDIST FILES
@@ -42,13 +42,13 @@ Module input
   Integer*4 :: number_accepted_points = 0 ! COUNT POINTS IN PARAMETER SPACE
   Integer*4 :: number_rejected_points = 0 ! COUNT POINTS IN PARAMETER SPACE
   Integer*4 :: weight = 1 ! COUNTS NUMBER OF TAKEN STEPS BEFORE MOVING TO A NEW POINT
-  Integer*4,parameter    :: jumping_factor_update = 250    ! STEPS TAKEN BEFORE UPDATING JUMPING FACTOR (IF NEEDED)
-  Integer*4,parameter    :: steps_taken_before_definite_run = 10000 !10000 !5000!10000!0 ! STEPS TAKEN BEFORE FREEZING COVARIANCE MATRIX
+  Integer*4,parameter    :: jumping_factor_update = 20    ! STEPS TAKEN BEFORE UPDATING JUMPING FACTOR (IF NEEDED)
+  Integer*4,parameter    :: steps_taken_before_definite_run = 50000 !10000 !5000!10000!0 ! STEPS TAKEN BEFORE FREEZING COVARIANCE MATRIX
   Integer*4,parameter    :: covariance_matrix_update = 2000 !5000!10000!0 ! STEPS TAKEN BEFORE UPDATING COVARIANCE MATRIX (IF NEEDED)
   Integer :: status1
   Integer*4,dimension(13) :: buff
   
-  Real*8,parameter       :: step_size_changes = 4.d-1      ! CHANGE IN STEP SIZE
+  Real*8,parameter       :: step_size_changes = 1.d-1      ! CHANGE IN STEP SIZE
   Real*8,dimension(number_of_parameters,number_of_parameters) :: Cov_mat ! COVARIANCE MATRIX
   Real*8,dimension(number_of_parameters) :: old_point, current_point, bestfit_point
   Real*8, allocatable, dimension(:,:) :: Nl ! SHOT NOISE
@@ -70,7 +70,7 @@ Module input
   Real*8,parameter :: N_ur = 2.0328d0
   Real*8,parameter :: N_ncdm = 1.d0
   Real*8,parameter :: deg_ncdm = 1.d0
-  Real*8,parameter :: tau = 5.96d-2
+  !Real*8,parameter :: tau = 5.96d-2
   !Real*8,parameter :: sigma_tau = 8.9d-3
 
   Integer*4,parameter :: nbins = 5
@@ -93,11 +93,11 @@ Module input
   Real*8,parameter :: s_2 = -0.0671d0
   Real*8,parameter :: s_3 = 0.1031d0
   
-  Character(len=*),parameter :: selection = 'tophat' ! OPTIONS: 'tophat', 'gaussian'
+  Character(len=*),parameter :: selection = 'gaussian' ! OPTIONS: 'tophat', 'gaussian'
 
   ! PATHS TO FILES:
 
-  Character(len=*),parameter :: ROOT_PATH = '/home/projects/dea/mcmc'
+  Character(len=*),parameter :: ROOT_PATH = '/home/projects/coupled-dm-de/mcmc'
   Character(len=*),parameter :: OUTPUT = ROOT_PATH//trim('/')//'output'
   Character(len=*),parameter :: CHAINS = ROOT_PATH//trim('/')//'chains'
   Character(len=*),parameter :: DATA = ROOT_PATH//trim('/')//'data'
