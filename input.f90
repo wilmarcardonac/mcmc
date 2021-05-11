3e5b067e78fd2573df6f2dacb32ff8b49674b3f0
Wilmar Alberto Cardona Castro <wilmar.cardona@unige.ch>
2021-05-11 11:14:36 -0500

    SHOULD WORK IN CIBIOFI

--- input.f90
+++ input.f90
@@ -30,10 +30,10 @@ Module input
 
   ! MCMC PARAMETERS 
   Character(len=*),parameter :: likelihood = 'euclid' ! OPTIONS: 'gaussian','euclid'
-  Character(len=*),parameter :: starting_point = 'mean' ! OPTIONS: 'mean','bestfit','random','last_point'
-  Character(len=*),parameter :: starting_cov_mat = 'diagonal' !'diagonal' ! OPTIONS: 'diagonal','given'
+  Character(len=*),parameter :: starting_point = 'bestfit' ! OPTIONS: 'mean','bestfit','random','last_point'
+  Character(len=*),parameter :: starting_cov_mat = 'given' !'diagonal' ! OPTIONS: 'diagonal','given'
   
-  Integer*4,parameter :: number_iterations = 1100!550000 ! TOTAL NUMBER OF ITERATIONS IN MCMC RUN
+  Integer*4,parameter :: number_iterations = 150000 ! TOTAL NUMBER OF ITERATIONS IN MCMC RUN
   Integer*4,parameter :: number_of_parameters = 11   ! TOTAL NUMBER OF VARYING PARAMETERS
   Integer*4,parameter :: number_of_prior_parameters = 8 ! NUMBER OF PARAMETERS WITH PRIOR     
   Integer*4,parameter :: UNIT_FILE1 = 80  ! UNIT NUMBER EXECUTION INFORMATION FILE
@@ -51,8 +51,8 @@ Module input
   Integer*4 :: number_rejected_points = 0 ! COUNT POINTS IN PARAMETER SPACE
   Integer*4 :: weight = 1 ! COUNTS NUMBER OF TAKEN STEPS BEFORE MOVING TO A NEW POINT
   Integer*4,parameter    :: jumping_factor_update = 20    ! STEPS TAKEN BEFORE UPDATING JUMPING FACTOR (IF NEEDED)
-  Integer*4,parameter    :: steps_taken_before_definite_run = 50000 !10000 !5000!10000!0 ! STEPS TAKEN BEFORE FREEZING COVARIANCE MATRIX
-  Integer*4,parameter    :: covariance_matrix_update = 1000!2000 !5000!10000!0 ! STEPS TAKEN BEFORE UPDATING COVARIANCE MATRIX (IF NEEDED)
+  Integer*4,parameter    :: steps_taken_before_definite_run = 0 !10000 !5000!10000!0 ! STEPS TAKEN BEFORE FREEZING COVARIANCE MATRIX
+  Integer*4,parameter    :: covariance_matrix_update = 2000 !5000!10000!0 ! STEPS TAKEN BEFORE UPDATING COVARIANCE MATRIX (IF NEEDED)
   Integer :: status1
   Integer*4,dimension(13) :: buff
   
@@ -64,8 +64,8 @@ Module input
   Real*8 :: jumping_factor = 2.38d0/sqrt(dble(number_of_parameters)) ! INCREASE/DECREASE TO MATCH INITIAL ACCEPTANCE PROBABILITY
   Real*8 :: old_loglikelihood,current_loglikelihood      ! STORE LIKELIHOOD VALUES
   Real*4,dimension(number_iterations) :: acceptance_probability
-  Real*4,parameter :: lower_limit_ap = 0.2!0.1! LOWER LIMIT ACCEPTANCE PROBABILITY
-  Real*4,parameter :: upper_limit_ap = 0.3!0.4! UPPER LIMIT ACCEPTANCE PROBABILITY
+  Real*4,parameter :: lower_limit_ap = 0.1! LOWER LIMIT ACCEPTANCE PROBABILITY
+  Real*4,parameter :: upper_limit_ap = 0.4! UPPER LIMIT ACCEPTANCE PROBABILITY
   Real*4 :: average_ap ! AVERAGE ACCEPTANCE PROBABILITY
 
   Logical :: bad_ap!,good_acceptance_probability ! CONTROL PLAUSIBLE VALUES OF COSMOLOGICAL PARAMETERS
@@ -107,7 +107,7 @@ Module input
 
   ! PATHS TO FILES:
 
-  Character(len=*),parameter :: ROOT_PATH = '/home/wcardona/projects/deaMI/t10nl/mcmc'
+  Character(len=*),parameter :: ROOT_PATH = '/home/projects/deaMI/mcmc'
   Character(len=*),parameter :: OUTPUT = ROOT_PATH//trim('/')//'output'
   Character(len=*),parameter :: CHAINS = ROOT_PATH//trim('/')//'chains'
   Character(len=*),parameter :: DATA = ROOT_PATH//trim('/')//'data'
